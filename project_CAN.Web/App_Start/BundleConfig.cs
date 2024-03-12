b8a163a323da64b03fef2b9f152339a829203d3b
Negru Nichita <nichita11062003@gmail.com>
2024-03-12 12:50:10 -0700

    Replaced default template with our web page design, added bootstrap icons

--- project_CAN.Web/App_Start/BundleConfig.cs
+++ project_CAN.Web/App_Start/BundleConfig.cs
@@ -18,26 +18,14 @@ namespace project_CAN
             //          "~/Content/bootstrap.css",
             //          "~/Content/site.css"));
 
-            // Script Bundles
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery/dist/jquery.min.js",
-                "~/Scripts/jquery-ui/jquery-ui.min.js", "~/Scripts/slimScroll/jquery.slimscroll.min.js",
-                "~/Scripts/jquery-flot/jquery.flot.js", "~/Scripts/jquery-flot/jquery.flot.resize.js",
-                "~/Scripts/jquery-flot/jquery.flot.pie.js", "~/Scripts/jquery.flot.spline/index.js",
-                "~/Scripts/homer.js"
-                ));
-            bundles.Add(new ScriptBundle("~/bundles/iCheck").Include("~/Scripts/iCheck/icheck.min.js"));
-            bundles.Add(new ScriptBundle("~/bundles/sparkline").Include("~/Scripts/sparkline/index.js"));
-            bundles.Add(new ScriptBundle("~/bundles/peity").Include("~/Scripts/peity/jquery.peity.min.js"));
 
             // Bootstrap Bundles
             bundles.Add(new Bundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap/dist/js/bootstrap.min.js"));
 
             // CSS Bundles
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                     "~/Content/style.css", "~/Content/font-awesome.css",
-                     "~/Content/helper.css", "~/Content/metisMenu.css",
-                     "~/Content/pe-icon-7-stroke.css", "~/Content/animate.css",
-                     "~/Content/bootstrap.css"));
+                     "~/Content/index.css", "~/Content/bootstrap.min.css",
+                     "~/Content/bootstrap-icons-1.11.3/font/bootstrap-icons.css"));
         }
     }
 }
