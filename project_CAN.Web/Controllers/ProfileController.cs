70d31ef525348207618c19d32a495bc8669ffca8
Cristian <smartcristian2002@gmail.com>
2024-03-27 21:41:39 +0200

    added in webConfig the authentification mode, modified usercontext

--- project_CAN.Web/Controllers/ProfileController.cs
+++ project_CAN.Web/Controllers/ProfileController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using project_CAN.Web.Extension;
+using project_CAN.Web.Models;
 
 namespace project_CAN.Web.Controllers
 {
-    public class ProfileController : Controller
+    public class ProfileController : BaseController
     {
         // GET: Profile
         public ActionResult Index()
         {
+            SessionStatus();
+            if ((string)System.Web.HttpContext.Current.Session["LoginStatus"] != "login")
+            {
+                return RedirectToAction("Index", "Login");
+            }
             return View();
         }
     }
---
+++ project_CAN.Web/Models/UserRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace project_CAN.Web.Models
+{
+    public class UserRegistration
+    {
+        [Required]
+        [Display(Name = "Username sau Email")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Parola")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Repeta Parola")]
+        public string RepeatPassword { get; set; }
+    }
+}
